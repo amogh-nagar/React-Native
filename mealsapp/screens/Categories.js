69e08b045fd331d9e57232f4ed206c391a9503a1
Amoghtech <amogh.1923cs1192@kiet.edu>
2021-11-08 19:40:08 +0530

    Meals page

--- mealsapp/screens/Categories.js
+++ mealsapp/screens/Categories.js
@@ -3,28 +3,48 @@ import {
   FlatList,
   Button,
   View,
+  Platform,
   Text,
   StyleSheet,
   TouchableOpacity,
+  TouchableNativeFeedback,
 } from "react-native";
 import { CATEGORIES } from "../data/dummydata";
 
 const Categories = (props) => {
+  let Touchablecmp = TouchableOpacity;
+
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    Touchablecmp = TouchableNativeFeedback;
+  }
+
   const rendergriditem = (itemdata) => {
     return (
-      <TouchableOpacity
-        onPress={() => {
-          props.navigation.navigate({
-            routeName: "CategoryMeals",
-            params: {
-              categoryid: itemdata.item.id,
-            },
-          });
-        }}
-        style={styles.griditem}
-      >
-        <Text>{itemdata.item.title}</Text>
-      </TouchableOpacity>
+      <View style={styles.griditem}>
+        
+        <Touchablecmp
+          onPress={() => {
+            props.navigation.navigate({
+              routeName: "CategoryMeals",
+              params: {
+                categoryid: itemdata.item.id,
+              },
+            });
+          }}
+          style={{ flex: 1 }}
+        >
+          <View
+            style={{
+              ...styles.container,
+              ...{ backgroundColor: itemdata.item.color },
+            }}
+          >
+            <Text style={styles.title} numberOfLines={2}>
+              {itemdata.item.title}
+            </Text>
+          </View>
+        </Touchablecmp>
+      </View>
     );
   };
 
@@ -50,10 +70,28 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  container: {
+    flex: 1,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 10,
+    elevation: 3,
+    padding: 10,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
   griditem: {
     flex: 1,
     margin: 15,
     height: 150,
+    overflow:'hidden'
+  },
+  title: {
+    fontFamily: "open-bold",
+    fontSize: 22,
+    textAlign: "right",
   },
 });
 export default Categories;
--- mealsapp/screens/CategoryMeals.js
+++ mealsapp/screens/CategoryMeals.js
@@ -1,29 +1,53 @@
 import React from "react";
-import { Button, View, Text, StyleSheet } from "react-native";
+import { Button, View, Text, StyleSheet, ImageBackground } from "react-native";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
 import { HeaderTitle } from "react-navigation-stack";
-import { CATEGORIES } from "../data/dummydata";
+import { CATEGORIES, MEALS } from "../data/dummydata";
 
 const CategoryMeals = (props) => {
   const catid = props.navigation.getParam("categoryid");
-  const category = CATEGORIES.find((c) => c.id === catid);
+  const displaymeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catid) >= 0
+  );
+
+  const mealrender = (itemdata) => {
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          props.navigation.navigate({});
+        }}
+        style={styles.mealitem}
+      >
+        <View>
+          <View style={{ ...styles.mealrow, ...styles.mealheader }}>
+            <ImageBackground
+              source={{ uri: itemdata.item.imageUrl }}
+              style={styles.bgimg}
+            >
+              <View style={styles.titlecnt}>
+                <Text numberOfLines={1} style={styles.title}>
+                  {itemdata.item.title}
+                </Text>
+              </View>
+            </ImageBackground>
+          </View>
+          <View style={{ ...styles.mealrow, ...styles.mealrowdetail }}>
+            <Text>{itemdata.item.duration}</Text>
+            <Text>{itemdata.item.complexity.toUpperCase()}</Text>
+            <Text>{itemdata.item.affordability}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View style={styles.screen}>
-      <Text>CategoryMeals</Text>
-      <Text>{category.title}</Text>
-      <Button
-        title="Go to Meal Detail"
-        onPress={() => {
-          props.navigation.navigate({
-            routeName: "MealDetail",
-          });
-        }}
-      />
-      <Button
-        title="Go back"
-        onPress={() => {
-          //   props.navigation.goBack()
-          props.navigation.pop();
-        }}
+      <FlatList
+        data={displaymeals}
+        renderItem={mealrender}
+        keyExtractor={(item, index) => item.id}
+        style={{ width: "100%" }}
       />
     </View>
   );
@@ -34,15 +58,49 @@ CategoryMeals.navigationOptions = (navigationdata) => {
   const category = CATEGORIES.find((c) => c.id === catid);
   return {
     headerTitle: category.title,
-    
   };
 };
 
 const styles = StyleSheet.create({
+  bgimg: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "flex-end",
+  },
   screen: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
+  mealrow: {
+    flexDirection: "row",
+  },
+  mealitem: {
+    borderRadius: 10,
+    height: 200,
+    width: "100%",
+    backgroundColor: "#ccc",
+  },
+  mealheader: {
+    height: "85%",
+  },
+  mealrowdetail: {
+    paddingHorizontal: 10,
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "15%",
+  },
+  titlecnt: {
+    paddingVertical: 5,
+    paddingHorizontal: 12,
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  title: {
+    color: "white",
+    fontFamily: "open-bold",
+    fontSize: 22,
+    textAlign: "center",
+  },
 });
+
 export default CategoryMeals;
